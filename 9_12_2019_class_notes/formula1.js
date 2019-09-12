f8a89ae6463fc3004b848e473e4d7ab12d890cec
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Pro.local>
2019-09-12 19:36:49 -0400

    Making our first formula

---
+++ 9_12_2019_class_notes/formula1.js
@@ -0,0 +1,13 @@
+// The formula for the LTV - Lifetime Value of a customer in business
+// LTV =  (ARPA * Gross Margin)/ CCR
+// ARPA: Average Revenue Per Account (The average MRR across all of your active customers)
+// Gross Margin: The difference between revenue and COGS (Cost Of Goods Sold). This is typically extremely high in SaaS (>80%)
+// Customer Churn Rate: The rate at which your customers are cancelling their subscriptions.]
+
+// var length = 10;
+// var width = 5;
+//
+// var area = width * length;
+//  console.log(area);
+
+console.log(5);
